2bdef45a82cb10aec0c3ddf5acc5caccfdcf929a
ma2808203259 <33137074+ma2808203259@users.noreply.github.com>
2019-05-06 23:49:15 +1000

    Update MainActivity.java

--- app/src/main/java/com/deakin/university/diverconsole/MainActivity.java
+++ app/src/main/java/com/deakin/university/diverconsole/MainActivity.java
@@ -1,13 +1,54 @@
 package com.deakin.university.diverconsole;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.os.Bundle;
+import android.widget.TextView;
+import java.text.SimpleDateFormat;
+
+import java.text.SimpleDateFormat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void onCreate(Bundle savedInstanceState)
+    {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+                Thread newthread = new Thread()
+                {
+                    @Override
+                    public void run()
+                    {
+                        try{
+                            while(!isInterrupted())
+                            {
+                                Thread.sleep(1000);
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        TextView tem = (TextView) findViewById(R.id.date);
+                                        long date = System.currentTimeMillis();
+                                        SimpleDateFormat sdf = new SimpleDateFormat("MMM/dd/yyyy\nhh:mm:ss a");
+                                        String s = sdf.format(date);
+                                        tem.setText(s);
+
+                                    }
+                                });
+                            }
+                        }
+                        catch (InterruptedException e)
+                        {}
+                    }
+                };
+                newthread.start();
     }
+
 }
