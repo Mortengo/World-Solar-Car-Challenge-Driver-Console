aeb5101e8c94449d065c142cbd8d4d4f4f3d30f7
Ray Guo <ma2808203259@hotmail.com>
2019-05-08 15:53:26 +1000

    Removed unused imports

--- app/src/main/java/com/deakin/university/diverconsole/MainActivity.java
+++ app/src/main/java/com/deakin/university/diverconsole/MainActivity.java
@@ -1,18 +1,8 @@
 package com.deakin.university.diverconsole;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
-import android.os.Bundle;
-import android.widget.TextView;
-import java.text.SimpleDateFormat;
-
 import java.text.SimpleDateFormat;
 
 public class MainActivity extends AppCompatActivity {
