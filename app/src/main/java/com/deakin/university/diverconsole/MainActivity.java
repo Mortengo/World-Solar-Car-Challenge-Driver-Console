8a75cf62b5feef0d0fad8d42ad59b07b08444106
Ray Guo <ma2808203259@hotmail.com>
2019-05-08 23:23:14 +1000

    Temperature

--- app/src/main/java/com/deakin/university/diverconsole/MainActivity.java
+++ app/src/main/java/com/deakin/university/diverconsole/MainActivity.java
@@ -3,42 +3,86 @@ package com.deakin.university.diverconsole;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
-import java.text.SimpleDateFormat;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.widget.Button;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView temp = null;
+    private SensorManager ManageSensor = null;
+    private Button show = null;
+    private Button hide = null;
+    private Sensor TemSensor = null;
+    private float temperatureC;
+
+    public float getTemperatureC() {
+        return getFloat(temperatureC) ;
+    }
+    public float getFloat(float value)
+    {
+        return (float)(Math.round(value * 10)) / 10;
+    }
+    public void setTemperatureC(float temperatureC)
+    {
+        this.temperatureC = temperatureC;
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-                super.onCreate(savedInstanceState);
-                setContentView(R.layout.activity_main);
-                Thread newthread = new Thread()
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        temp = (TextView)findViewById(R.id.Tem);
+        //get the sensor server
+        ManageSensor = (SensorManager)getSystemService(SENSOR_SERVICE);
+        //give the sensor type of
+        TemSensor = ManageSensor.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        show = (Button)findViewById(R.id.showTem);
+        hide = (Button)findViewById(R.id.hideTem);
+        final SensorEventListener mSensorEventListner = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                if (event.sensor.getType() == Sensor.TYPE_AMBIENT_TEMPERATURE)
                 {
-                    @Override
-                    public void run()
-                    {
-                        try{
-                            while(!isInterrupted())
-                            {
-                                Thread.sleep(1000);
-                                runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        TextView tem = (TextView) findViewById(R.id.date);
-                                        long date = System.currentTimeMillis();
-                                        SimpleDateFormat sdf = new SimpleDateFormat("MMM/dd/yyyy\nhh:mm:ss a");
-                                        String s = sdf.format(date);
-                                        tem.setText(s);
+                    float Temperature = event.values[0];
+                    setTemperatureC(Temperature);
+                    temp.setText(String.valueOf(getTemperatureC() + "℃"));
+                }
+            }
 
-                                    }
-                                });
-                            }
-                        }
-                        catch (InterruptedException e)
-                        {}
-                    }
-                };
-                newthread.start();
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+        show.setOnClickListener(new Button.OnClickListener()
+        {
+            @Override
+            public void onClick(View arg0)
+            {
+                ManageSensor.registerListener(mSensorEventListner, TemSensor,SensorManager.SENSOR_DELAY_NORMAL);
+                show.setVisibility(View.INVISIBLE);
+                hide.setVisibility(View.VISIBLE);
+                temp.setVisibility(View.VISIBLE);
+            }
+        });
+        hide.setOnClickListener(new Button.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                ManageSensor.unregisterListener(mSensorEventListner,TemSensor);
+                hide.setVisibility(View.INVISIBLE);
+                show.setVisibility(View.VISIBLE);
+                temp.setVisibility(View.INVISIBLE);
+            }
+        });
+
     }
 
 }
